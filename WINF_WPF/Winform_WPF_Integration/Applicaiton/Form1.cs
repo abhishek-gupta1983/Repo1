f9802c016af2da4df68717d06a9aecc76e169675
abhishek-gupta1983 <guptaabhishek@gmail.com>
2014-03-13 10:44:33 +0530

    Winform WPF integration

---
+++ WINF_WPF/Winform_WPF_Integration/Applicaiton/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace Applicaiton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /* our WPF control */
+            WPFControl.UserControl2 uc1 = new WPFControl.UserControl2();
+
+            /* container that will host our WPF control, we set it using 
+             * the Child property */
+            ElementHost host = new ElementHost()
+            {
+                BackColor = Color.Transparent,
+                Child = uc1,
+                Dock = DockStyle.Fill,
+            };
+
+            /* now add the ElementHost to our controls collection 
+             * normally */
+            Controls.Add(host);
+        }
+    }
+}
